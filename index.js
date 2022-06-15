3b01caff9480f695391b627ccee7f522342bbde8
George Baroi <baroi.george@gmail.com>
2022-06-14 20:19:25 -0400

    commit tree a3688a1411f4e8561e9d2dbce8d775d87d7970d1

---
+++ index.js
@@ -0,0 +1,31 @@
+function add(){}
+function subtract(){}
+function divide(){}
+function multiply(){}
+
+function add(a , b){
+    return a + b
+}
+function subtract(a , b){
+    return a -b
+}function multiply(a , b){
+    return a * b
+}
+function divide(a , b){
+    return a / b
+}
+function increment(n){
+    return ++n
+}
+function decrement(n){
+    return --n
+}
+parseInt("n" , 10)
+function makeInt(n){
+    return parseInt(n , 10)
+}
+parseFloat("n")
+function preserveDecimal(n){
+    return parseFloat(n)
+}
+
